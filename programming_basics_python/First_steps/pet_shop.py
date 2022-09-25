11e7c62222c83c40dae4ed81387b68031f73430c
gajev <113999980+gajev@users.noreply.github.com>
2022-09-25 10:13:37 +0300

    uploading_first_steps

---
+++ programming_basics_python/First_steps/pet_shop.py
@@ -0,0 +1,6 @@
+packdog = int(input()) * 2.5
+packcat = int(input()) * 4
+suma = (packdog + packcat)
+
+print (f' {suma} lv.')
+
---
+++ programming_basics_python/First_steps/yard_greening.py
@@ -0,0 +1,10 @@
+square = float(input())
+priceone = 7.61
+price = square * priceone
+discount = 0.18 * price
+end = price - discount
+
+print (f' The final price is: {end} lv.')
+print (f' The discount is: {discount} lv.')
+
+
